0822048cba2e31e647cd7df82dad940b2c2ddc0c
ashkan <kotksthegame@gmail.com>
2022-12-09 10:28:34 +0330

    - Improve main file.

--- main.py
+++ main.py
@@ -15,27 +15,29 @@ finder = CenterFinder(
     in_source,
     GeneticAlgorithm(
         selection.AverageFitnessSelection(),
-        crossover.StrNptCrossover(1),
+        crossover.StrNptCrossover(10),
         mutation.StrBitFlippingMutation(),
         replacement.AlphaGenerationalReplacement(0.3),
     ),
 )
 
-print(config.CHROMOSOME_LENGTH)
 finder.draw_target_points([[1, 0, 0]])
 end_flag, res_point = finder.check_end_condition()
 
 while not end_flag:
-    print(f'----------------------------------- GENERATION {finder.get_limit()} -----------------------------------')
-    finder.run_cycle(True)
+    print(f'------------------------------ GENERATION {finder.get_limit() + 1} ------------------------------')
+    finder.run_cycle(draw_middle=False, draw_current=False)
     finder.draw_best([[0, 0, 1]])
     end_flag, res_point = finder.check_end_condition()
-    if finder.get_limit() == 20:
+    if finder.get_limit() == config.MAX_GENERATION:
+        print('Algorithm reached to maximum possible generation.')
         finder.clear_middle_points()
         break
 
 best_point = finder.get_best()
 print('Best point:', best_point.to_arr())
 print('Best point fitness:', finder.get_best_in_genotype().calc_fitness(finder._target_points))
+print('Distance from targets are:', [t.calc_distance(best_point) for t in finder._target_points])
 
-time.sleep(60)
+for _ in range(6000):
+    finder._drawer.flush()
