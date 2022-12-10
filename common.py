efaa173b1e896118849f0b6ff0cfdc368f0b05f3
ashkan <kotksthegame@gmail.com>
2022-12-11 02:29:16 +0330

    - Implement calibration in common module.

--- common.py
+++ common.py
@@ -3,12 +3,14 @@ from gui.drawers.matplot import MatplotDrawer1D, MatplotDrawer2D, MatplotDrawer3
 from gui.point import Point
 import time
 import config
+import math
 
 DRAWERS = {
     1: MatplotDrawer1D,
     2: MatplotDrawer2D,
     3: MatplotDrawer3D
 }
+_CALIBRATED = False
 
 
 def print_active_config():
@@ -74,3 +76,60 @@ def float_to_real(f: float) -> int:
         return res - 1
 
     return res
+
+
+def is_calibrated() -> bool:
+    return _CALIBRATED
+
+
+def get_calibration_status() -> typing.Tuple[bool, bool, bool, bool]:
+    set_chromosome_len = True
+    set_gnome_len = True
+    set_lower_bounds = True
+    set_upper_bounds = True
+
+    if config.CHROMOSOME_LENGTH is not None:
+        set_chromosome_len = False
+    else:
+        config.CHROMOSOME_LENGTH = 0
+
+    if config.GENOME_LEN:
+        set_gnome_len = False
+    if config.LOWER_BOUND:
+        set_lower_bounds = False
+    if config.UPPER_BOUND:
+        set_upper_bounds = False
+
+    return set_chromosome_len, set_gnome_len, set_upper_bounds, set_lower_bounds
+
+
+def calibrate(points: typing.List[Point]):
+    global _CALIBRATED
+    points_arr = point_to_arr(points)
+    dimension = points[0].get_dimension()
+    calibrate_status = get_calibration_status()
+    for i in range(dimension):
+        l_bound = find_min(points_arr, i)
+        u_bound = find_max(points_arr, i)
+        l_bound -= l_bound / 10 * 2
+        u_bound += u_bound / 10 * 2
+        if calibrate_status[3]:
+            config.LOWER_BOUND.append(l_bound)
+        if calibrate_status[2]:
+            config.UPPER_BOUND.append(u_bound)
+        g_len = math.ceil(math.log2(u_bound - l_bound + 1))
+        if calibrate_status[1]:
+            config.GENOME_LEN.append(g_len)
+        if calibrate_status[0]:
+            config.CHROMOSOME_LENGTH += g_len
+
+    if config.INITIAL_POPULATION is None:
+        config.INITIAL_POPULATION = math.ceil(1.65 * (2 ** (0.21 * config.CHROMOSOME_LENGTH)))
+
+    if config.INITIAL_POPULATION % 2 != 0:
+        config.INITIAL_POPULATION += 1
+
+    if config.TARGET_TOLERANCE is None:
+        config.TARGET_TOLERANCE = 0.001
+
+    _CALIBRATED = True
